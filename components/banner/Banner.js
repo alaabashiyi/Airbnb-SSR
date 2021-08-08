4f9f05e1bdb94294650e601d40c3da2040e1e95a
alaabashiyi <alaa.bashiy@gmail.com>
2021-08-09 00:31:40 +0300

    date picker and searchbox

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-const Banner = () => {
+const Header = () => {
     return (
         <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
             <Image src="https://links.papareact.com/0fm" layout="fill" objectFit="cover" />
@@ -12,4 +12,4 @@ const Banner = () => {
     )
 }
 
-export default Banner;
+export default Header;
---
+++ components/searchinput/SearchInput.js
@@ -0,0 +1,12 @@
+import { SearchIcon } from '@heroicons/react/solid'
+
+const SearchInput = ({ search, setSearch }) => {
+    return (
+        <>
+            <input value={search} onChange={(e) => setSearch(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400" type='search' placeholder='Start your search' />
+            <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+        </>
+    )
+}
+
+export default SearchInput
